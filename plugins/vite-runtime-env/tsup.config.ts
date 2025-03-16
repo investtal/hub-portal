fc19c76f9c5cfd6a1473e4a5fcd9751bf60164d6
harrytran998 <nhattq.coding@gmail.com>
2025-03-16 13:52:41 +0700

    feat(vite-runtime): use esm only

--- plugins/vite-runtime-env/tsup.config.ts
+++ plugins/vite-runtime-env/tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup"
 export default defineConfig({
   clean: true,
   dts: true,
-  format: ["esm", "cjs"],
+  format: ["esm"],
   entry: ["src/index.ts"],
+  target: "es2022",
 })
