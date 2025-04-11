8daad58423c2ed09a4bc57fea8f1a0ee81c4ec41
harrytran998 <nhattq.coding@gmail.com>
2025-04-11 20:32:50 +0700

    IVT-76 fix(toolkit): update code comment from code rabbit

--- packages/toolkit/src/browser/calculateScrollPercentage/index.ts
+++ packages/toolkit/src/browser/calculateScrollPercentage/index.ts
@@ -4,7 +4,7 @@ import { isBrowser } from "../../predicate/isBrowser"
 // @__NO_SIDE_EFFECTS__
 export function calculateScrollPercentage(): number {
   invariant(isBrowser())
-  const scrollPosition = window.scrollY || window.scrollY || document.documentElement.scrollTop
+  const scrollPosition = window.scrollY || document.documentElement.scrollTop
   const totalHeight = document.documentElement.scrollHeight - window.innerHeight
   const scrollPercentage = (scrollPosition / totalHeight) * 100
   return scrollPercentage
--- packages/toolkit/src/object/removeEmptyObject/index.ts
+++ packages/toolkit/src/object/removeEmptyObject/index.ts
@@ -3,13 +3,13 @@ import { isNotEmpty } from "../../predicate/isNotEmpty"
 import { isNotNull } from "../../predicate/isNotNull"
 
 /**
- * @description Remove empty fields from an object. This will mutates the object
+ * @description Remove empty fields from an object and returns a new filtered object
  * @param {any} obj - The input obj
  * @returns {any} - the clean obj
  */
 // @__NO_SIDE_EFFECTS__
-export function removeEmptyObj<T extends Record<string, any>>(obj: T): T {
+export function removeEmptyObj<T extends Record<string, any>>(obj: T): Partial<T> {
   return Object.fromEntries(
     Object.entries(obj).filter(([_, v]) => isNotNull(v) && isDefined(v) && isNotEmpty(v)),
-  ) as T
+  ) as Partial<T>
 }
