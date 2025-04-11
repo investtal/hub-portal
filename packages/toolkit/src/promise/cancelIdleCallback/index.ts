721fd41ed5cecbf44a37ed319b225064408ac7bd
harrytran998 <nhattq.coding@gmail.com>
2025-04-11 20:19:56 +0700

    IVT-76 chore(toolkit): remove unnecessary promise function

--- packages/toolkit/src/promise/cancelIdleCallback/index.ts
+++ packages/toolkit/src/promise/cancelIdleCallback/index.ts
@@ -1,9 +1,9 @@
-function cancelIdleCallbackShim(id: any) {
+function cancelIdleCallbackShim(id: any): void {
   clearTimeout(id)
 }
 
 // @__NO_SIDE_EFFECTS__
-export const cancelIdleCallback =
+export const cancelIdleCallback: (id: any) => void =
   typeof window !== "undefined"
     ? window.cancelIdleCallback || cancelIdleCallbackShim
     : cancelIdleCallbackShim
