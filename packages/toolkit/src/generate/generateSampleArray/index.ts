da6ac1847d0842b320fb71ec085417e61dd38913
harrytran998 <nhattq.coding@gmail.com>
2025-04-11 17:20:56 +0700

    IVT-76 feat(toolkit): update generate function return type

--- packages/toolkit/src/generate/generateSampleArray/index.ts
+++ packages/toolkit/src/generate/generateSampleArray/index.ts
@@ -2,6 +2,6 @@
  * Get random items from an array
  */
 // @__NO_SIDE_EFFECTS__
-export function generateSample<T>(arr: T[], count: number) {
+export function generateSample<T>(arr: T[], count: number): T[] {
   return Array.from({ length: count }, _ => arr[Math.round(Math.random() * (arr.length - 1))])
 }
