93f535316f090af9789727d4b3b223158959f9b5
harrytran998 <nhattq.coding@gmail.com>
2025-05-25 08:27:02 +0700

    release toolkit 1.2.0

--- packages/toolkit/tsdown.config.ts
+++ packages/toolkit/tsdown.config.ts
@@ -4,17 +4,12 @@ import pkg from "./package.json"
 const external = [...Object.keys(pkg.devDependencies)]
 
 export default defineConfig({
-  entry: [
-    "src/index.ts",
-    "src/file/**/*.ts",
-    "src/git/**/*.ts",
-    "src/stream/**/*.ts",
-    "!**/*.test.ts",
-  ],
+  entry: ["src/index.ts", "src/**/**/*.ts", "!**/*.test.ts"],
   format: ["esm"],
   target: "es2022",
   dts: { transformer: "oxc" },
   external,
   sourcemap: true,
+  treeshake: true,
   clean: true,
 })
